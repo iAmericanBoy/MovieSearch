412f97ef443ae96efc1bafc99bc4362977751ff4
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-08 10:45:10 -0700

    Adds UIElements to Code

--- MovieSearch/Controller/View Controller/MovieListViewController.swift
+++ MovieSearch/Controller/View Controller/MovieListViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class MovieListViewController: UIViewController {
     //MARK: - Outlets
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     //MARK: - Properties
-
+    var movies:[Movie] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         MovieController.fetchMovieWith(name: "Star Wars") { (movieList) in
-            print(movieList)
+            self.movies = movieList
         }
     }
     
@@ -32,4 +45,17 @@ class MovieListViewController: UIViewController {
     }
     */
 
+}
+//MARK: - TableViewDataSource, TableViewDelegate
+extension MovieListViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell
+        
+        cell?.movie = movies[indexPath.row]
+        return cell ?? UITableViewCell()
+    }
 }
---
+++ MovieSearch/Controller/View Controller/MovieTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MoviewTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Dominic Lanzillotta on 2/8/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    //MARK: - Outlets
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    //MARK: - Properties
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    //MARK: - Private Functions
+    private func updateViews() {
+        guard let movie = movie else {return}
+        nameLabel.text = movie.title
+        ratingLabel.text = "Rating: \(movie.rating ?? 0)"
+        summaryLabel.text = movie.overview
+    }
+    
+}
