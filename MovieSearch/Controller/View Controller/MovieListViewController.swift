4bb4536a64262fc74949a3cdfb84dbf3589b6f52
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-08 12:08:18 -0700

    Final Code CleanUp

--- MovieSearch/Controller/View Controller/MovieListViewController.swift
+++ MovieSearch/Controller/View Controller/MovieListViewController.swift
@@ -32,18 +32,6 @@ class MovieListViewController: UIViewController {
         searchBar.delegate = self
 
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 //MARK: - TableViewDataSource, TableViewDelegate
@@ -67,7 +55,6 @@ extension MovieListViewController: UITableViewDataSource, UITableViewDelegate{
             cell?.movie = movies[indexPath.row]
             return cell ?? UITableViewCell()
         }
-
     }
 }
 
